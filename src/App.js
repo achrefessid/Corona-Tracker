529ca8de45d3d276d2f158b4ee0b2d3bbd5ee581
Achref <Essid>
2020-11-22 17:34:38 +0100

    update

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends React.Component {
 
     return (
       <>
+      {/*navbar*/}
       <nav class="navbar navbar-dark default-color"><a class="navbar-brand" href="#">Corona Virus Tracker</a>
       <ul class="navbar-nav ml-auto nav-flex-icons">
       <li class="nav-item">
@@ -37,7 +38,21 @@ class App extends React.Component {
       </li>
       </ul>
       </nav>
-      
+      {/*navbar*/}
+        <header id="header">
+        <div className="logo">
+        <a class="navbar-brand" href="#">Corona Virus Tracker</a>
+        </div>
+        <nav id="nav-bar">
+          <ul>
+          <li><a className="nav-link" to="/"></a></li>
+          <a class="nav-link waves-effect waves-light"><i class="fab fa-facebook"></i></a>
+          <a class="nav-link waves-effect waves-light"><i class="fab fa-twitter"></i></a>
+          </ul>
+        </nav>
+      </header>
+
+      {/**/}
       <div className={styles.container}>
         <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
@@ -45,11 +60,15 @@ class App extends React.Component {
         <Chart data={data} country={country} />
         </div>
         
-        <footer class="page-footer font-small teal">
+      {/*footer*/}
+        {/*<footer class="page-footer font-small teal">
           <div class="footer-copyright text-center py-3">© 2020 Copyright:
           <a href=""> Corona Virus Tracker</a>
           </div>
-        </footer>
+          </footer>*/}
+          <footer className="footer">
+          <p>© 2020 Copyright: &copy; Corona Virus Tracker</p>
+          </footer>
         </>
     );
   }
